1518c4f1b35bf564ef426dc10f05f63fa8c349ab
Plamen Stoilkov <sdplamen@gmail.com>
2025-04-11 17:41:26 +0300

    Fix changes.

--- infiniting/profil/views.py
+++ infiniting/profil/views.py
@@ -9,13 +9,13 @@ def profile_view(request):
     return render(request, 'profil/profil.html')
 
 def group_list_view(request):
-    return render(request, 'profil/group_list.html')
+    return render(request, 'profil/group.html')
 
 def article_list_view(request):
-    return render(request, 'profil/article_list.html')
+    return render(request, 'profil/article.html')
 
 def auction_list_view(request):
-    return render(request, 'profil/auction_list.html')
+    return render(request, 'profil/auction.html')
 
 def author_detail_view(request, author_id):
     return render(request, 'profil/author_detail.html')
