1aa1f07db8520da0d4e0a1580856460967803cfa
Plamen Stoilkov <sdplamen@gmail.com>
2025-04-12 09:51:18 +0300

    Fix changes.

--- infiniting/infiniting/settings.py
+++ infiniting/infiniting/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
             ],
         },
     },
@@ -118,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'profil', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # For deployment
