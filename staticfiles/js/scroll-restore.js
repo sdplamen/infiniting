21310443d0db51eb3ee7d5422e69a286155bf120
Plamen Stoilkov <sdplamen@gmail.com>
2025-08-13 14:19:02 +0300

    Update style pagination

--- staticfiles/js/scroll-restore.js
+++ staticfiles/js/scroll-restore.js
@@ -1,9 +1,12 @@
-    window.addEventListener('beforeunload', () => {
+window.addEventListener('beforeunload', () => {
     sessionStorage.setItem('scrollpos', window.scrollY);
     sessionStorage.setItem('scrollpath', window.location.pathname);
 });
 
 document.addEventListener('DOMContentLoaded', () => {
+    // The following code is commented out to prevent the page from restoring the scroll position on refresh.
+    // The page will now scroll to the top on every refresh.
+    /*
     const scrollpos = sessionStorage.getItem('scrollpos');
     const scrollpath = sessionStorage.getItem('scrollpath');
     if (scrollpos && scrollpath === window.location.pathname) {
@@ -11,4 +14,5 @@ document.addEventListener('DOMContentLoaded', () => {
         sessionStorage.removeItem('scrollpos');
         sessionStorage.removeItem('scrollpath');
     }
+    */
 });
