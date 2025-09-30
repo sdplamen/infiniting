aa89b9a5399bc70c14955e41a7f6bea3da60caeb
Plamen Stoilkov <sdplamen@gmail.com>
2025-09-30 15:54:15 +0300

    Update pagination for profiles

--- users/mixins.py
+++ users/mixins.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+class UserIsProfileOwnerMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user == self.get_object().user
+
+
 class PaginationMixin:
     max_pages_to_show = 3
 
